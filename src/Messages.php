18ea496bb00432f12ca3bdf1d72c6b6b91ec92d9
Kasper Garnaes <kasper.garnaes@gmail.com>
2019-08-30 09:01:48 +0200

    Dummy change

--- src/Messages.php
+++ src/Messages.php
@@ -13,7 +13,7 @@ class Messages
 
     public function message() : string
     {
-        return "With love from Platform.sh";
+        return "With love from Platform.sh! Authenticated by GitHub!";
     }
 
 }
